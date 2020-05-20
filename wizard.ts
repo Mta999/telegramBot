c67e5ba8d617342c09606e48cb2519f9e9d3f611
Mher Tum <mher.tumanyan99@mail.ru>
2020-05-20 14:02:57 +0400

    added multiple answers

--- wizard.ts
+++ wizard.ts
@@ -1,83 +1,74 @@
-// import WizardScene from 'telegraf/scenes/wizard';
-// import { Extra, Markup } from 'telegraf';
-// const WizardScene = require('telegraf/scenes/wizard');
-
-
-
-// const chooseTime =  Markup.inlineKeyboard([
-//   Markup.callbackButton('11:00','11:00'),
-//   Markup.callbackButton('12:00','12:00'),
-//   Markup.callbackButton('13:00','13:00'),
-//   Markup.callbackButton('Choose other time','Choose other time')
-//     ]).oneTime()
-//     .resize();
-
-
-// export const superWizard = new WizardScene('super-wizard',
-//  async (ctx) => {
-//    console.log('aaaaaaaaaaaaaaaaaaaaaaaaa');
-   
-//      await ctx.telegram.sendMessage(796175303,'When you started your working day yesterday ?', Extra.markup(chooseTime))
-//      console.log(2222222);
-//      // ctx-update-callbackquery-data
-//      console.log('++++++++++++++++++++++++++',ctx.wizard.ctx) //undefined
-//     console.log('------------------------',ctx);
-//     return ctx.wizard.next();
-//   },
-//   (ctx) => {
-//     ctx.reply('Step 3');
-//     return ctx.wizard.next();
-//   },
-//   (ctx) => {
-//     ctx.reply('Step 4');
-//     return ctx.wizard.next();
-//   },
-//   (ctx) => {
-//     ctx.reply('Done');
-//     return ctx.scene.leave();
-//   }
-// );
-
 const Telegraf = require('telegraf');
 const session = require('telegraf/session');
 const Stage = require('telegraf/stage');
 const WizardScene = require('telegraf/scenes/wizard');
 import { Extra, Markup } from 'telegraf';
 
-const chooseTime = Markup.keyboard([
-    ['11:00', '12:00'],
-    ['13:00', 'Choose other time']
-   ]).oneTime()
-   .resize();
-   
-   const chooseNumbers = Markup.keyboard([
-    ['1', '2'],
-    ['5', '10']
-   ]).oneTime()
-   .resize();
+const tasks = [];
 
+const chooseTime: Markup = Markup.keyboard([
+  ['11:00', '12:00'],
+  ['13:00', 'Choose other time']
+]).oneTime()
+  .resize();
 
+const chooseNumbers = Markup.keyboard([
+  ['1', '2'],
+  ['5', '10']
+]).oneTime()
+  .resize();
 
+const chooseProjects = Markup.keyboard([
+  ['4nextLab', 'bitCluster'],
+  ['Blot', 'KW'],
+  ['Other, please write which']
+]).oneTime()
+  .resize();
+
+  
+const chooseProjectOrNo = Markup.keyboard([
+  ['4nextLab', 'bitCluster'],
+  ['Blot', 'KW'],
+  ['No']
+]).oneTime()
+  .resize();
+  
 export const superWizard = new WizardScene(
   'super-wizard',
-   (ctx) => {
-      ctx.telegram.sendMessage(796175303,'Երբ ես երեկ սկսել գործ անել՞ ', Extra.markup(chooseTime))
-    ctx.wizard.state.data = {};    
+  (ctx) => {
+    ctx.telegram.sendMessage(796175303, 'Երբ ես երեկ սկսել գործ անել՞ ', Extra.markup(chooseTime))
+    ctx.wizard.state.data = {};
     return ctx.wizard.next();
   },
   ctx => {
-      console.log(ctx);
-      
-    ctx.wizard.state.data.time = ctx.message.text;
-    ctx.telegram.sendMessage(796175303,'երեկ քանի հատ թասկ եք արել՞', Extra.markup(chooseNumbers))
+    ctx.wizard.state.data.workTime = ctx.message.text;
+    ctx.telegram.sendMessage(796175303, 'երեկ քանի հատ թասկ եք արել՞', Extra.markup(chooseNumbers))
     return ctx.wizard.next();
   },
   ctx => {
-
-    ctx.wizard.state.data.number = ctx.message.text;
-
-    ctx.reply(`երեկ սկսել ես գործ անել ${ctx.wizard.state.data.time}-ին ու արել ես  ${ctx.wizard.state.data.number} հատ թասք `);
+    ctx.wizard.state.data.tasksCounter = ctx.message.text;
+    ctx.telegram.sendMessage(796175303, 'ինչ պրոյեկտների վրա ես աշխատել', Extra.markup(chooseProjects))
+    return ctx.wizard.next();
+  },
+  ctx => {
+    // ctx.wizard.state.data.whatTask = ctx.message.text;
+    tasks.push(ctx.message.text)
+    ctx.telegram.sendMessage(796175303, 'բա բացի դրանցից՞', Extra.markup(chooseProjects))
+    //  console.log('55555555555', ctx.wizard.state.data);
+    // return ctx.scene.leave();
+    return ctx.wizard.next();
+  },
+  ctx => {
+    tasks.push(ctx.message.text);
+    ctx.wizard.state.data.whatTasks = tasks;
+    ctx.telegram.sendMessage(796175303, 'եթե էլի կա, նշեք, եթե չէ՝ սեղմեք NO', Extra.markup(chooseProjectOrNo))
+    console.log('55555555555', ctx.wizard.state.data);
     return ctx.scene.leave();
+    // return ctx.wizard.next();
+  },
+  ctx =>{
+    
   }
 );
 
+// return ctx.scene.leave();
